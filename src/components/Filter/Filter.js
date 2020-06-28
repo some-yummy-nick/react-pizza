eb7323401f2a8131aad67916752eb6919af40329
Marat <yeffasol@mail.ru>
2020-06-28 17:55:27 +0300

    Добавляет появление меню при hover и в фокусе

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 const filters = [
     {
+        id: 0,
         title: "all",
         text: "Все"
     },
     {
+        id: 1,
         title: "meat",
         text: "Мясные"
     },
     {
+        id: 2,
         title: "vegetarian",
         text: "Вегетарианская"
     },
     {
+        id: 3,
         title: "grill",
         text: "Гриль"
     },
     {
+        id: 4,
         title: "spice",
         text: "Острые"
     },
     {
+        id: 5,
         title: "closed",
         text: "Закрытые"
     },
@@ -32,8 +38,9 @@ export const Filter = () => <>
         <ul className="filter__items reset-list">
             {
                 filters.map(filter =>
-                    <li className="filter__item">
-                        <button className="filter__button reset-btn" data-name={filter.title} type="button">{filter.text}</button>
+                    <li className="filter__item" key={`filter-${filter.id}`}>
+                        <button className="filter__button reset-btn" data-name={filter.title}
+                                type="button">{filter.text}</button>
                     </li>
                 )
             }
--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -2,34 +2,40 @@ import React from 'react';
 
 const sorts = [
     {
+        id: 0,
         title: "popular",
         text: "популярности"
     },
     {
+        id: 1,
         title: "price",
         text: "по цене"
     },
     {
+        id: 2,
         title: "alphabet",
         text: "по алфавиту"
     },
 ];
 export const Sort = () =>
     <div className="sort">
-        <div className="sort__title ">
-            <svg width="10" height="6" className="icon icon_down sort__icon">
-                <use xlinkHref="#down"/>
-            </svg>
-            Сортировка по: <button type="button" className="sort__selected reset-btn">популярности</button></div>
-        <ul className="sort__list reset-list">
-            {
-                sorts.map(sort =>
-                    <li className="sort__item">
-                        <button type="button" className="sort__button reset-btn">{sort.text}</button>
-                    </li>
-                )
-            }
-        </ul>
+        <div className="sort__wrapper">
+            <div className="sort__title">
+                <svg width="10" height="6" className="icon icon_down sort__icon">
+                    <use xlinkHref="#down"/>
+                </svg>
+                Сортировка по: <button type="button" id="dropdown-title"
+                                       className="sort__selected reset-btn">популярности</button></div>
+            <ul className="sort__list reset-list" aria-labelledby="dropdown-title">
+                {
+                    sorts.map(sort =>
+                        <li className="sort__item" key={`sort-${sort.id}`}>
+                            <button type="button" className="sort__button reset-btn">{sort.text}</button>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
     </div>;
 
 export default Sort;
